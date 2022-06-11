0bdaba0c3e3b6c675015d1f5309f443a66f50f48
TIAGO DA SILVA SOUZA <72765859+titicos@users.noreply.github.com>
2022-06-11 08:59:54 -0400

    firstcommit

---
+++ main.js
@@ -0,0 +1,79 @@
+var peso= document.getElementById("peso").value;
+var altura= document.getElementById("altura").value;
+
+
+
+function calculoImc(x,y){
+
+    var imc = x/(y*2)
+
+    return imc
+}
+
+function limpar(){
+
+    if(peso.value!=="" && altura.value!==""){
+      document.getElementById("peso").value=""
+      document.getElementById("altura").value=""
+      document.getElementById("resultado").value=""
+  
+    }
+    
+  
+  }
+
+
+function calcularImc(){
+   
+    var peso= document.getElementById("peso").value;
+    var altura= document.getElementById("altura").value;
+
+    if(peso=="" || altura=="") {
+        alert("Preencha todos os campos")
+        var entradas = document.getElementById("peso")
+        var entrada = document.getElementById("altura")
+        document.getElementById("resultado").innerHTML=" "
+
+        entradas.style.backgroundColor="#ee9984"
+        entrada.style.backgroundColor="#ee9984"
+    } 
+
+    else{
+        var resultado =  calculoImc(peso,altura)
+        if(resultado<10){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Fora dos padrões IMC."
+        }
+
+        if(resultado>10 && resultado<=18.5){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Atenção,está abaixo do peso."
+        }
+
+        if(resultado>=18.5 && resultado<=24.9){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" peso normal."
+        }
+
+        if(resultado>=25 && resultado<=29.9){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Excesso de peso."
+        }
+
+        if(resultado>=30 && resultado<=34.9){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Obesidade Classe I."
+        }
+
+        if(resultado>=35 && resultado<=39.9){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Obesidade Classe II."
+        }
+
+        if(resultado>=40){
+            document.getElementById("resultado").innerHTML="IMC: "+resultado.toFixed(2) +" Obesidade Classe III."
+        }
+        
+    }
+
+
+   
+    
+
+ 
+
+}
